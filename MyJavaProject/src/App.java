fe00ff51f4d375b244ebf799360bb30069f6efdc
Sohel Rana <mdsrana11@gmail.com>
2023-12-23 23:49:28 +0600

    Developed different methods

--- MyJavaProject/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        for (int i = 1; i <= 10; i++) {
-            System.err.println(i);
-        }
-    }
-}
--- MyJavaProject/src/hello.java
+++
@@ -1,6 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-        String name = "Sohel Rana";
-        System.out.println(name);
-    }
-}
